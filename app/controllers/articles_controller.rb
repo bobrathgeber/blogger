d14ae442c639b064cdbeafbe22bb0d345d492113
Bob Rathgeber <bob.rathgeber@gmail.com>
2016-10-11 12:50:53 -0500

    Add Flash messages for the form.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     #body: params[:article][:body])
     @article = Article.new(article_params)
     @article.save
-
+    flash.notice = "Article '#{@article.title}' Created!"
     redirect_to article_path(@article)
   end
 
@@ -31,12 +31,15 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(article_params)
 
+    flash.notice = "Article '#{@article.title}' Updated!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+    flash.notice = "Article '#{@article.title}' was Deleted!"
     redirect_to articles_path
   end
 end
