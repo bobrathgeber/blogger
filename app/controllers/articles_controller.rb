a6c95ad00652a10ae86d72cf1d280bf0b271e356
Bob Rathgeber <bob.rathgeber@gmail.com>
2016-10-11 12:21:10 -0500

    Add basics: Create, Edit, Read, Destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
   end
 
   def show
@@ -8,14 +14,29 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    #@article = Article.new(
+    #title: params[:article][:title],
+    #body: params[:article][:body])
     @article = Article.new(article_params)
     @article.save
-    redirect_to @article
+
+    redirect_to article_path(@article)
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
 
-  private
-  def article_params
-    params.require(:post).permit(:title, :body)
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
   end
 end
